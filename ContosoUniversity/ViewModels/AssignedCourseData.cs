7c30b7c9c9461fdea213fb5baf0c890d62fb4aa0
Alex Bogomol <alexander.bogomol@gmail.com>
2015-07-01 20:59:47 +0300

    Simplify multi-check-widget for instructors courses

--- ContosoUniversity/ViewModels/AssignedCourseData.cs
+++ ContosoUniversity/ViewModels/AssignedCourseData.cs
@@ -5,5 +5,15 @@
         public int CourseId { get; set; }
         public string Title { get; set; }
         public bool Assigned { get; set; }
+
+        public string OptionText
+        {
+            get { return string.Format("[{0}] {1}", CourseId, Title); }
+        }
+
+        public string OptionCheckedAttr
+        {
+            get { return Assigned ? "checked=\"checked\"" : ""; }
+        }
     }
 }

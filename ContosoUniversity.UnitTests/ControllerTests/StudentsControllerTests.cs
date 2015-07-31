15d1d8173bd99c75c748483ff4c4cb1e6494499a
Alex Bogomol <alexbogomol@github.com>
2015-07-31 23:55:45 +0300

    UnitTesting Project introduced and all pkgs updates

---
+++ ContosoUniversity.UnitTests/ControllerTests/StudentsControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using ContosoUniversity.Controllers;
+using ContosoUniversity.DataAccess.Contracts;
+using ContosoUniversity.Models;
+using Moq;
+using NUnit.Framework;
+using System.Linq;
+using TestStack.FluentMVCTesting;
+
+namespace ContosoUniversity.UnitTests.ControllerTests
+{
+    [TestFixture]
+    public class StudentsControllerTests
+    {
+        IQueryable<Student> studentslist;
+        Mock<IStudentsRepository> studentsrepo;
+        Mock<ISchoolUow> uow;
+        StudentController controller;
+
+        [TestFixtureSetUp]
+        public void Initialize()
+        {
+            studentslist = (new Student[]
+            {
+                new Student { Id = 1, LastName = "Student1" },
+                new Student { Id = 2, LastName = "Student2" },
+                new Student { Id = 3, LastName = "Student3" },
+                new Student { Id = 4, LastName = "Student4" },
+                new Student { Id = 5, LastName = "Student5" }
+            }).AsQueryable();
+
+            studentsrepo = new Mock<IStudentsRepository>();
+            studentsrepo.Setup(repo => repo.GetAll())
+                        .Returns(studentslist);
+
+            uow = new Mock<ISchoolUow>();
+            uow.Setup(uow => uow.Students).Returns(studentsrepo.Object);
+
+            controller = new StudentController(uow.Object);
+        }
+
+        [Test]
+        public void ShouldRenderDefaultView()
+        {
+            controller.WithCallTo(c => c.Index(null, null, null, null))
+                      .ShouldRenderDefaultView();
+        }
+    }
+}
--- ContosoUniversity/Config/NinjectWebCommon.cs
+++ ContosoUniversity/Config/NinjectWebCommon.cs
@@ -3,27 +3,29 @@
 
 namespace ContosoUniversity.Config
 {
-    using Infrastructure.Ninject;
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-    using Ninject;
-    using Ninject.Web.Common;
     using System;
     using System.Web;
 
-    public static class NinjectWebCommon 
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+    using Infrastructure.Ninject;
+
+    public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -31,7 +33,7 @@ namespace ContosoUniversity.Config
         {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>

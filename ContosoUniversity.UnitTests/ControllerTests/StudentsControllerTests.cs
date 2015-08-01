84cfa379e048d9d7598c33b566d07cc8a62e6ca7
Alex Bogomol <alexbogomol@github.com>
2015-08-01 15:44:35 +0300

    StudentController Paging Tests

--- ContosoUniversity.UnitTests/ControllerTests/StudentsControllerTests.cs
+++ ContosoUniversity.UnitTests/ControllerTests/StudentsControllerTests.cs
@@ -1,9 +1,11 @@
 ﻿using ContosoUniversity.Controllers;
 using ContosoUniversity.DataAccess.Contracts;
 using ContosoUniversity.Models;
+using ContosoUniversity.ViewModels.Students;
 using Moq;
 using NUnit.Framework;
 using System.Linq;
+using System.Web.Mvc;
 using TestStack.FluentMVCTesting;
 
 namespace ContosoUniversity.UnitTests.ControllerTests
@@ -39,10 +41,39 @@ namespace ContosoUniversity.UnitTests.ControllerTests
         }
 
         [Test]
-        public void ShouldRenderDefaultView()
+        public void IndexShouldRenderDefaultView()
         {
             controller.WithCallTo(c => c.Index(null, null, null, null))
                       .ShouldRenderDefaultView();
+        }
+
+        [Test]
+        public void IndexShouldRenderFirstPage()
+        {
+            var result = controller.Index(null, null, null, null) as ViewResult;
+
+            var viewmodel = result.Model as StudentsListViewModel;
+
+            Assert.That(viewmodel.StudentsList.PageNumber == 1);
+            Assert.That(viewmodel.StudentsList.PageCount == 2);
+            Assert.That(viewmodel.StudentsList.PageSize == 3);
+            Assert.That(viewmodel.StudentsList[0].Id == 1);
+            Assert.That(viewmodel.StudentsList[1].Id == 2);
+            Assert.That(viewmodel.StudentsList[2].Id == 3);
+        }
+
+        [Test]
+        public void IndexShouldRenderSecondPage()
+        {
+            var result = controller.Index(null, null, null, 2) as ViewResult;
+
+            var viewmodel = result.Model as StudentsListViewModel;
+
+            Assert.That(viewmodel.StudentsList.PageNumber == 2);
+            Assert.That(viewmodel.StudentsList.PageCount == 2);
+            Assert.That(viewmodel.StudentsList.PageSize == 3);
+            Assert.That(viewmodel.StudentsList[0].Id == 4);
+            Assert.That(viewmodel.StudentsList[1].Id == 5);
         }
     }
 }

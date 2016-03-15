01e210a57bce212139f7aeb93637096c5e28f051
Alex Bogomol <alexbogomol@github.com>
2016-03-15 17:51:59 +0200

    Involve the DataAccessModule for Autofac

--- ContosoUniversity/Config/AutofacConfig.cs
+++ ContosoUniversity/Config/AutofacConfig.cs
@@ -1,10 +1,6 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
-using ContosoUniversity.DataAccess;
-using ContosoUniversity.DataAccess.Contracts;
-using ContosoUniversity.DataAccess.Repositories;
-using ContosoUniversity.Models;
-using System.Data.Entity;
+using ContosoUniversity.Infrastructure.Autofac;
 using System.Reflection;
 using System.Web.Mvc;
 
@@ -32,27 +28,11 @@ namespace ContosoUniversity.Config
             // OPTIONAL: Enable property injection into action filters.
             builder.RegisterFilterProvider();
 
-            RegisterDataAccessModule(builder);
+            builder.RegisterModule<DataAccessModule>();
 
             // Set the dependency resolver to be Autofac.
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-        }
-
-        private static void RegisterDataAccessModule(ContainerBuilder builder)
-        {
-            builder.RegisterType<SchoolContext>().As<DbContext>().InstancePerRequest();
-
-            builder.RegisterType<CoursesRepository>().As<ICoursesRepository>().InstancePerRequest();
-            builder.RegisterType<StudentsRepository>().As<IStudentsRepository>().InstancePerRequest();
-            builder.RegisterType<InstructorsRepository>().As<IInstructorsRepository>().InstancePerRequest();
-            builder.RegisterType<EnrollmentsRepository>().As<IEnrollmentsRepository>().InstancePerRequest();
-            builder.RegisterType<DepartmentsRepository>().As<IAsyncRepository<Department>>().InstancePerRequest();
-
-            builder.RegisterGeneric(typeof(EfRepository<>)).As(typeof(IRepository<>)).InstancePerRequest();
-            builder.RegisterGeneric(typeof(EfAsyncRepository<>)).As(typeof(IAsyncRepository<>)).InstancePerRequest();
-
-            builder.RegisterType<SchoolUow>().As<ISchoolUow>().InstancePerRequest();
         }
     }
 }
